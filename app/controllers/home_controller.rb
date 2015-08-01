41c12eb1d650a2e9564d8ce68419f0958e2965f1
Josh Langholtz <josh.langholtz@validic.com>
2015-08-01 19:20:47 -0400

    Add index page for news articles and link from home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @images = Comfy::Cms::File.all
-    @articles = Comfy::Cms::Page.where('label ~* ?', 'news').last(3)
+    @articles = Comfy::Cms::Page.where('label ~* ?', 'news').last(3).reverse
     render cms_layout: 'base', cms_blocks: {
       carousel: { template: '/home/index' },
       news: { partial: '/home/news' }
