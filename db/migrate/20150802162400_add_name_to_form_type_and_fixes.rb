62f71164285c00bd0ed2fa8447ef7a5f5593b156
Josh Langholtz <josh.langholtz@validic.com>
2015-08-02 12:53:25 -0400

    Comment out failing migration.

--- db/migrate/20150802162400_add_name_to_form_type_and_fixes.rb
+++ db/migrate/20150802162400_add_name_to_form_type_and_fixes.rb
@@ -1,8 +1,8 @@
 class AddNameToFormTypeAndFixes < ActiveRecord::Migration
-  def change
-    add_column :form_types, :name, :string
-    remove_column :form_type_questions, :adoption_form_type_id, :integer
-    remove_column :submissions, :adoption_form_type_id, :integer
-    remove_column :responses, :adoption_form_id, :integer
-  end
+#   def change
+#     add_column :form_types, :name, :string
+#     remove_column :form_type_questions, :adoption_form_type_id, :integer
+#     remove_column :submissions, :adoption_form_type_id, :integer
+#     remove_column :responses, :adoption_form_id, :integer
+#   end
 end
