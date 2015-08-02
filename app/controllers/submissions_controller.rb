ecdd3f461f8fc47525bf1ca218e97eb0654adba9
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-08-02 11:50:08 -0400

    form submitting

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,7 +1,8 @@
 class SubmissionsController < ApplicationController
   def new
-    @submission = Submission.new(form_type_id: 1)
-    @questions = @submission.form_type.questions
+    @submission = Submission.new_with_build_answers( 1 )
+    # @submission = Submission.new(form_type_id: 1)
+    # @submission.responses.build
   end
 
   def create
