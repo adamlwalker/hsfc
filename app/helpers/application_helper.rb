fee6018404f5e53f9c10f2fa8ff3204105f8ef36
maebeale <maebeale@gmail.com>
2015-11-01 20:19:29 -0500

    helpers referenced from submission form views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  require "#{Rails.root}/app/helpers/submissions_helper.rb"
+  include SubmissionsHelper
+
+  require "#{Rails.root}/app/helpers/us_states_helper.rb"
+  include UsStatesHelper
+
+  def params_controller_name
+    params[:controller]
+  end
 end
